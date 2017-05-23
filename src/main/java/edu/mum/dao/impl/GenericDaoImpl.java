e2d8a2efa3a2a3b276812bb9d2d69414229e0186
edyluisrey <eaguirre378@gmail.com>
2017-05-23 11:06:53 -0500

    [FEATURE]: support ehcache with AOP

---
+++ src/main/java/edu/mum/dao/impl/GenericDaoImpl.java
@@ -0,0 +1,63 @@
+package edu.mum.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import edu.mum.dao.GenericDao;
+
+
+/*@SuppressWarnings("unchecked")
+@Repository*/
+public abstract class GenericDaoImpl<T> implements GenericDao<T> {
+
+	@PersistenceContext
+    protected EntityManager entityManager;
+
+    protected Class<T> daoType;
+
+	public void setDaoType(Class<T> type) {
+			daoType = type;
+	}
+   
+    @Override
+    public void save( T entity ){
+        entityManager.persist( entity );
+     }
+
+    public void delete( T entity ){
+        entityManager.remove( entity );
+     }
+
+	@Override
+	public void delete(Long id) {
+        T entity = findOne( id );
+        delete( entity );  
+    }
+
+	@Override
+	public T findOne( Long id ){
+	    return (T) entityManager.find( daoType, id );
+	 }
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findAll(){
+		      return entityManager.createQuery( "from " + daoType.getName() )
+		       .getResultList();
+		   }
+	
+	@Override
+	public T update( T entity ){
+	      return entityManager.merge( entity );
+	   }
+
+ 	@Override
+	public List<T> findAll(String s,Object  hint ){
+ 	    return (List<T>)  entityManager.createQuery("SELECT m FROM Member AS m") 
+ 	    		.setHint(s,hint).getResultList();
+  	}
+	
+
+ }
