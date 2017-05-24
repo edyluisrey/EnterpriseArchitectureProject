39dccc4252e1a0786905a4f5acd5c26e421a4f8a
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-05-24 14:05:38 -0500

    Add authorization for Address service by using both Annocation Preauthorizzaiont and Secured

--- src/main/java/edu/mum/service/AddressService.java
+++ src/main/java/edu/mum/service/AddressService.java
@@ -2,20 +2,23 @@ package edu.mum.service;
 
 import java.util.List;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import edu.mum.domain.Address;
 
 public interface AddressService {
-
+	@PreAuthorize("ROLE_ADMIN")
 	public void save(Address address);
-	
+	@PreAuthorize("ROLE_ADMIN")
 	public void update(Address address);
-	
+	@Secured("ROLE_ADMIN")
 	public void delete(Address address);
-	
+	@Secured("ROLE_ADMIN")
 	public void deleteById(Long addressId);
-	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	public Address findAddressById(Long addressId);
-	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	public List<Address> findAll();
 	
 }
