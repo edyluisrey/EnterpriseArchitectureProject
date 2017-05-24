9d6fad8f09668b8f84f5b26bf63b9420f6d7ac57
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-05-24 13:50:17 -0500

    Add authoriza for Roome Service interface

--- src/main/java/edu/mum/service/RoomService.java
+++ src/main/java/edu/mum/service/RoomService.java
@@ -2,20 +2,22 @@ package edu.mum.service;
 
 import java.util.List;
 
+import org.springframework.security.access.annotation.Secured;
+
 import edu.mum.domain.Room;
 
 public interface RoomService {
-
+	
 	public void save(Room Room);
 	
 	public void update(Room Room);
 	
 	public void delete(Room Room);
-	
+	@Secured("ROLE_ADMIN")
 	public void deleteById(Long RoomId);
 	
 	public Room findRoomById(Long RoomId);
-	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	public List<Room> findAll();
 	
 }
