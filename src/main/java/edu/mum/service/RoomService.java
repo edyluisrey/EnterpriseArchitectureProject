27875bd427454ccae44e9176494dd37b0ccc9a62
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-05-24 13:57:44 -0500

    add PreAuthorize for Roomservice - the second solution for authorizaition

--- src/main/java/edu/mum/service/RoomService.java
+++ src/main/java/edu/mum/service/RoomService.java
@@ -3,15 +3,17 @@ package edu.mum.service;
 import java.util.List;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import edu.mum.domain.Room;
 
 public interface RoomService {
 	
 	public void save(Room Room);
-	
+	@PreAuthorize("ROLE_ADMIN")
 	public void update(Room Room);
 	
+	@PreAuthorize("ROLE_ADMIN")
 	public void delete(Room Room);
 	@Secured("ROLE_ADMIN")
 	public void deleteById(Long RoomId);
